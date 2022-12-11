bf8399adce5ca7245cd5a5774fafdb2df36de690
chengzihang <czhcheng27@gmail.com>
2022-12-11 19:09:22 +0800

    import antd form for login page

--- oa_syetem_client/src/pages/login/index.jsx
+++ oa_syetem_client/src/pages/login/index.jsx
@@ -1,8 +1,23 @@
 import React, { useState, useEffect, useRef } from "react";
+import { Button, Checkbox, Form, Input } from "antd";
 import Logo from "../../assets/logo.png";
 import css from "./index.module.css";
 
+const { Item } = Form;
+
 const Login = (props) => {
+  const layout = {
+    labelCol: { span: 6 },
+    wrapperCol: { span: 16 },
+  };
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
     <div className={css.wrapper}>
       {/* header */}
@@ -12,7 +27,58 @@ const Login = (props) => {
       </header>
 
       {/* login Form */}
-      <div className={css.login_form}>login Form</div>
+      <div className={css.login_form}>
+        <header>User Login</header>
+        <div className={css.form}>
+          <Form
+            name="basic"
+            {...layout}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Item
+              label="Username"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your username!",
+                },
+              ]}
+            >
+              <Input />
+            </Item>
+
+            <Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password />
+            </Item>
+
+            <Item
+              wrapperCol={{
+                offset: 6,
+                span: 16,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Item>
+          </Form>
+        </div>
+      </div>
     </div>
   );
 };
