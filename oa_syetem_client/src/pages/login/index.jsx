a019393bc8a6d349758254307394f100593cf760
chengzihang <czhcheng27@gmail.com>
2022-12-11 18:36:10 +0800

    login page css layout

--- oa_syetem_client/src/pages/login/index.jsx
+++ oa_syetem_client/src/pages/login/index.jsx
@@ -1,14 +1,20 @@
-import React, {
-  useState,
-  useEffect,
-  useCallback,
-  useRef,
-  useMemo,
-} from "react";
-import css from "./index.module.less";
+import React, { useState, useEffect, useRef } from "react";
+import Logo from "../../assets/logo.png";
+import css from "./index.module.css";
 
 const Login = (props) => {
-  return <div>Login</div>;
+  return (
+    <div className={css.wrapper}>
+      {/* header */}
+      <header className={css.header}>
+        <img src={Logo} alt="logo" />
+        <h1>React Project: Office Admin System &amp; Function Examples</h1>
+      </header>
+
+      {/* login Form */}
+      <div className={css.login_form}>login Form</div>
+    </div>
+  );
 };
 
 export default Login;
