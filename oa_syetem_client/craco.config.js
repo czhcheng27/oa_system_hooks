da10bb52d471cf5fb67be5a4ca6005fb70c7eab7
chengzihang <czhcheng27@gmail.com>
2023-03-01 22:23:40 +0800

    setting config path @

--- oa_syetem_client/craco.config.js
+++ oa_syetem_client/craco.config.js
@@ -1,6 +1,12 @@
 const CracoLessPlugin = require("craco-less");
+const path = require("path");
 
 module.exports = {
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   //按需引入
   babel: {
     plugins: [
