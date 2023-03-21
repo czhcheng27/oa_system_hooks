7c67d50e904592578a1fa169edcee9d1b921ee06
chengzihang <czhcheng27@gmail.com>
2023-03-21 23:22:11 +0800

    update moveAction

--- oa_syetem_client/src/pages/comps/revision/components/areaLeft/moveAction/index.jsx
+++ oa_syetem_client/src/pages/comps/revision/components/areaLeft/moveAction/index.jsx
@@ -11,9 +11,11 @@ import Down from "../imgs/down.png";
 import DownDis from "../imgs/downDis.png";
 import Delete from "../imgs/delete.png";
 import css from "./index.module.less";
+import { findUpperObj } from "../../../../../../utils";
 
 const MoveAction = ({
-  delChapter,
+  upDisNum = null, //从第几个的index开始禁止上移
+  delChildNode,
   selectData,
   activeOutline,
   setActiveOutline,
@@ -21,16 +23,19 @@ const MoveAction = ({
   clickDotIconFn,
   setBtnClicked,
 }) => {
+  const { id: selId } = selectData;
   const outlineAllData = useSelector((s) => s.rdcOutlineAllData);
-  const cntId = outlineAllData.findIndex((el) => el.id === "content");
+  const curOutline = findUpperObj(outlineAllData, selId); // 当前的外层大纲对象
+  const curIdNo = outlineAllData.findIndex((el) => el.id === curOutline.id);
 
   const visivleChange = (visible) => {
     !visible && eliminateDotIcon();
   };
 
   const renderContent = () => {
-    const upDis = selectData.id == 4;
-    const dnDis = selectData.id == outlineAllData[cntId].children.length;
+    const selIdNo = curOutline.children.findIndex((el) => el.id == selId); // 当前选择的子节点的index No.
+    const upDis = selIdNo == (upDisNum ?? 0); // 如果无输入的特殊需求（无 upDisNum），则第一位禁止上移
+    const dnDis = selIdNo + 1 === outlineAllData[curIdNo].children.length; // 最后一位禁止下移
     return (
       <div className={css.content_wrapper}>
         <div
@@ -50,7 +55,7 @@ const MoveAction = ({
         <PopCom
           title={"确定删除吗"}
           handleConfirm={(e) => (
-            delChapter(), setBtnClicked(false), e.stopPropagation()
+            delChildNode(), setBtnClicked(false), e?.stopPropagation()
           )}
         >
           <div>
@@ -69,15 +74,15 @@ const MoveAction = ({
 
   const moveAction = (e, action) => {
     e.stopPropagation();
-    const arr = outlineAllData[cntId].children;
-    const idx = arr.findIndex((el) => el.id == selectData.id);
+    const arr = outlineAllData[curIdNo].children; // 当前大纲的 children 子节点
+    const idx = arr.findIndex((el) => el.id == selId); // 选中子节点的 index No.
     if (action == "up") {
       swapArray(arr, idx - 1, idx);
     } else if (action == "down") {
       swapArray(arr, idx, idx + 1);
     }
     eliminateDotIcon(); // 进行完移动操作后清除尾部三个点的icon图标
-    resortIdx(outlineAllData[cntId].children); // 对数组内各个对象的index及name重新赋值
+    resortIdx(outlineAllData[curIdNo].children, curOutline.id); // 对数组内各个对象的index及name重新赋值
     const independent = independentComps.includes(activeOutline.id);
     !independent && setActiveOutline(selectData); // 如果当前激活的 outline 是正文内容则需要 setActiveOutline
   };
