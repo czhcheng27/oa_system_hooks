cf5b2afaba9e956014bb82318a65e3d5433d5be7
chengzihang <czhcheng27@gmail.com>
2023-03-22 10:53:21 +0800

    repalceFile develop

--- oa_syetem_client/src/pages/comps/revision/components/preface/index.jsx
+++ oa_syetem_client/src/pages/comps/revision/components/preface/index.jsx
@@ -9,6 +9,7 @@ import React, {
 import BasisStd from "./basisStd";
 import FileRel from "./fileRel";
 import css from "./index.module.less";
+import ReplaceFile from "./replaceFile";
 
 const Preface = forwardRef(({ prefaceData }, ref) => {
   const {
@@ -27,6 +28,7 @@ const Preface = forwardRef(({ prefaceData }, ref) => {
     <div className={css.preface_wrapper}>
       <BasisStd ref={basisStdRef} basisStd={basisStd} />
       <FileRel ref={fileRelRef} fileRel={fileRel} />
+      <ReplaceFile ref={replaceFileRef} replaceFile={replaceFile} />
     </div>
   );
 });
--- oa_syetem_client/src/pages/comps/revision/components/preface/replaceFile/index.jsx
+++ oa_syetem_client/src/pages/comps/revision/components/preface/replaceFile/index.jsx
@@ -8,31 +8,36 @@ import { Form, Input } from "antd";
 import classNames from "classnames";
 import { PlusCircleOutlined, MinusCircleOutlined } from "@ant-design/icons";
 import { cloneDeep, createUidKey } from "@/utils";
-import { inputToNum } from "@/utils";
 import CmTinymce from "../../../cmTinymce";
 import SwitchWrapper from "../../../switchWrapper";
 import PopCom from "../../../popCom";
+import { toolkitInit } from "../../../const";
 import css from "./index.module.less";
 
 const { Item } = Form;
 
 const initContent = [
   {
-    partNo: "",
-    contentTxt: "",
-    id: "initContentId",
+    symbol: "a",
+    value: "",
+    id: "replaceFileId1",
   },
 ];
 
+const contentObj = {
+  symbol: "",
+  value: "",
+};
+
 // eslint-disable-next-line react/display-name
-const FileRel = forwardRef(({ fileRel }, ref) => {
+const ReplaceFile = forwardRef(({ replaceFile }, ref) => {
   const [infoForm] = Form.useForm();
 
   const [valueData, setValueData] = useState(initContent);
   const [isSwitchOn, setIsSwitchOn] = useState(true);
 
   useImperativeHandle(ref, () => ({
-    getFileRel: () => {
+    getReplaceFile: () => {
       const infoData = infoForm.getFieldsValue();
       const data = { infoData, valueData, switchStatus: isSwitchOn };
       return data;
@@ -40,67 +45,79 @@ const FileRel = forwardRef(({ fileRel }, ref) => {
   }));
 
   useEffect(() => {
-    infoForm.setFieldsValue(fileRel.infoData ?? "");
-    setValueData(fileRel.valueData ?? initContent);
-    setIsSwitchOn(fileRel.switchStatus);
-  }, [fileRel]);
+    infoForm.setFieldsValue(replaceFile.infoData ?? "");
+    setValueData(replaceFile.valueData ?? initContent);
+    setIsSwitchOn(replaceFile.switchStatus);
+  }, [replaceFile]);
 
-  const valueChange = (value, obj, name) => {
+  // type 1 : 第一层级（a级添加）
+  const contentAdd = (data) => {
+    const curIndex = valueData.findIndex((el) => el.id === data.id);
+    const newData = cloneDeep(valueData);
+    const newLable = String.fromCharCode(data.symbol.charCodeAt(0) * 1 + 1);
+    newData.splice(curIndex + 1, 0, {
+      ...contentObj,
+      symbol: newLable,
+      id: createUidKey(),
+    });
+    if (valueData.length > 1) {
+      const afterArr = newData.slice(curIndex + 2);
+      afterArr.forEach((el) => {
+        el.symbol = String.fromCharCode(el.symbol.charCodeAt(0) * 1 + 1);
+        return el;
+      });
+    }
+    setValueData(newData);
+  };
+
+  // type 1 : 第一层级（a级删除）
+  const contentDel = (data) => {
+    const curIndex = valueData.findIndex((el) => el.id === data.id);
+    const newData = cloneDeep(valueData);
+    const preArr = newData.slice(0, curIndex);
+    const afterArr = newData.slice(curIndex + 1);
+    afterArr.forEach((el) => {
+      el.symbol = String.fromCharCode(el.symbol.charCodeAt(0) * 1 - 1);
+      return el;
+    });
+    setValueData([...preArr, ...afterArr]);
+  };
+
+  // 一级文本更改时更新数据函数
+  const contentTxtChange = (txt, obj) => {
     const newData = cloneDeep(valueData);
     newData.map((el) => {
-      if (el.id === obj.id) el[name] = value;
+      if (el.id === obj.id) el.value = txt;
       return el;
     });
     setValueData(newData);
   };
 
-  const btnAction = (data, action) => {
-    const curIndex = valueData.findIndex((el) => el.id === data.id);
-    const newData = cloneDeep(valueData);
-    if (action === "add") {
-      newData.splice(curIndex + 1, 0, {
-        ...initContent[0],
-        id: createUidKey(),
-      });
-    } else if (action === "del") {
-      newData.splice(curIndex, 1);
-    }
-    setValueData(newData);
-  };
-
   const renderContent = () => {
-    const disable = valueData.length === 1;
     return (
-      !!valueData.length &&
+      valueData.length &&
       valueData.map((el, index) => {
+        const disable = valueData.length === 1;
         return (
           <div key={index} className={css.content_wrapper}>
             <div className={css.content}>
-              <p>
-                No.
-                <Input
-                  value={el.partNo}
-                  onChange={(e) =>
-                    valueChange(inputToNum(e.target.value), el, "partNo")
-                  }
-                />
-              </p>
+              <p>{`${el.symbol})`}</p>
               <CmTinymce
+                value={el.value}
                 inline={true}
-                value={el.contentTxt}
-                selectorName="fileRel_content"
-                onChange={(data) => valueChange(data, el, "contentTxt")}
+                selectorName="replaceFile_bot_content"
+                onChange={(data) => contentTxtChange(data, el)}
               />
               <div className={css.symbol}>
                 {index + 1 === valueData.length ? "。" : ";"}
               </div>
               <div className={css.handle_btn}>
-                <PlusCircleOutlined onClick={() => btnAction(el, "add")} />
+                <PlusCircleOutlined onClick={() => contentAdd(el)} />
                 <PopCom
                   position={"left"}
                   title={"确定删除?"}
                   disable={disable}
-                  handleConfirm={() => !disable && btnAction(el, "del")}
+                  handleConfirm={() => contentDel(el)}
                 >
                   {
                     <MinusCircleOutlined
@@ -118,13 +135,13 @@ const FileRel = forwardRef(({ fileRel }, ref) => {
 
   return (
     <SwitchWrapper
-      label="File Relations"
+      label="Replace File Relations"
       switchVal={isSwitchOn}
       switchFunc={setIsSwitchOn}
     >
       <div className={css.top_wrapper}>
         <Form form={infoForm} labelCol={{ flex: "60px" }} autoComplete="off">
-          <div>This file is</div>
+          <div>This document supersedes</div>
           <div className={css.second_row}>
             <Item name="stdNo">
               <Input placeholder="Please enter the standard number" />
@@ -133,25 +150,19 @@ const FileRel = forwardRef(({ fileRel }, ref) => {
               <Input placeholder="Please enter the standard name" />
             </Item>
           </div>
-          <div className={css.third_row}>
-            's No.
-            <Item name="partNo">
-              <Input />
-            </Item>{" "}
-            part
-          </div>
+          <div className={css.third_row}>compared with</div>
           <div className={css.fourth_row}>
             <Item name="publishedNo">
               <Input placeholder="Please enter the standard number" />
             </Item>
-            <span>The following sections have been released：</span>
+            <span>the main technical changes are as following:</span>
           </div>
         </Form>
-      </div>
 
-      <div className={css.bot_wrapper}>{renderContent()}</div>
+        <div className={css.bot_wrapper}>{renderContent()}</div>
+      </div>
     </SwitchWrapper>
   );
 });
 
-export default FileRel;
+export default ReplaceFile;
