ab8cfe92642396a855cfe462e0b510b88f0d875a
chengzihang <czhcheng27@gmail.com>
2023-03-29 16:18:52 +0800

    scroll item wrapper

--- oa_syetem_client/src/pages/stage/stageOne/components/scrollItem/index.jsx
+++ oa_syetem_client/src/pages/stage/stageOne/components/scrollItem/index.jsx
@@ -1,15 +1,72 @@
 import React, { useState } from "react";
 import { Popover } from "antd";
+import StageFlow from "../../../../../components/StageFlow";
 import css from "./index.module.less";
 
 const ScrollItem = ({ data, warnDay }) => {
   const [dis, setDis] = useState("-50%");
+  const [nextPopoverVisible, setNextPopoverVisible] = useState(false);
+  const [prevPopoverVisible, setPrevPopoverVisible] = useState(false);
 
   return (
     <div className={css.scrollItem}>
       <div className={css.area} style={{ transform: `translate(${dis}, 0)` }}>
-        <div className={css.applyFor}>applyFor</div>
-        <div className={css.implement}>implement</div>
+        <div className={css.applyFor}>
+          <Popover
+            placement="topRight"
+            overlayClassName={css.popover}
+            title=""
+            content="展示实施阶段节点"
+            open={nextPopoverVisible}
+            onOpenChange={(visible) => {
+              setNextPopoverVisible(visible);
+            }}
+          >
+            <span
+              className={css.nextBtn}
+              onClick={() => {
+                setNextPopoverVisible(false);
+                setDis("-50%");
+              }}
+            ></span>
+          </Popover>
+          <StageFlow
+            data={data.ciBusinessUnitDTOList}
+            formalApprovalDate={data.formalApprovalDate}
+            setChangeStatus={data["approvalProgress"] == 3 ? true : false}
+            setChangeType={data.isUrgent}
+            warnNum={warnDay}
+            // isHover={isHover}
+          ></StageFlow>
+        </div>
+        <div className={css.implement}>
+          <Popover
+            placement="top"
+            overlayClassName={css.popover}
+            title=""
+            content="展示申请阶段节点"
+            open={prevPopoverVisible}
+            onOpenChange={(visible) => {
+              setPrevPopoverVisible(visible);
+            }}
+          >
+            <span
+              className={css.prevBtn}
+              onClick={() => {
+                setPrevPopoverVisible(false);
+                setDis(0);
+              }}
+            ></span>
+          </Popover>
+          <StageFlow
+            data={data.ciBusinessUnitDTOList}
+            formalApprovalDate={data.formalApprovalDate}
+            setChangeStatus={data["status"] == 3 ? true : false}
+            setChangeType={data.isUrgent}
+            warnNum={warnDay}
+            //   isHover={isHover}
+          ></StageFlow>
+        </div>
       </div>
     </div>
   );
