7754c226fb1ae74df24cb5b491a9ca950631c64a
chengzihang <czhcheng27@gmail.com>
2023-04-10 23:48:43 +0800

    infoDetail

---
+++ oa_syetem_client/src/pages/stage/stageOne/components/infoDetail/index.jsx
@@ -0,0 +1,14 @@
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  useRef,
+  useMemo,
+} from "react";
+import css from "./index.module.less";
+
+const InfoDetail = (props) => {
+  return <div className={css.box_wrap}>InfoDetail</div>;
+};
+
+export default InfoDetail;
