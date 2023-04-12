fd9561ae64978b7f2dcfe513c9c72a726e699cea
chengzihang <czhcheng27@gmail.com>
2023-04-13 00:28:43 +0800

    titleParts comp

---
+++ oa_syetem_client/src/pages/stage/stageOne/components/titlePart/index.jsx
@@ -0,0 +1,14 @@
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  useRef,
+  useMemo,
+} from "react";
+import css from "./index.module.less";
+
+const TitleParts = (props) => {
+  return <div>TitleParts</div>;
+};
+
+export default TitleParts;
