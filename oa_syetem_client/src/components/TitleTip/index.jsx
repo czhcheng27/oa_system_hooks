bcd23e9635a49779128aacbbb3c22ea702e35c06
chengzihang <czhcheng27@gmail.com>
2023-04-13 15:33:09 +0800

    titleTips comp

---
+++ oa_syetem_client/src/components/TitleTip/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Icon from "./imgs/iconTip.png";
+import css from "./index.module.less";
+
+const TitleTip = ({
+  children,
+  height = "24px",
+  icon = Icon,
+  fontWeight = "600",
+  fontSize = "16px",
+}) => {
+  return (
+    <div className={css.titleTip_wrapper} style={{ height }}>
+      {/* return <div className={css.infoSplit}> */}
+      <div>
+        <img src={icon} />
+      </div>
+      <div style={{ fontWeight, fontSize }}>{children}</div>
+    </div>
+  );
+};
+
+export default TitleTip;
