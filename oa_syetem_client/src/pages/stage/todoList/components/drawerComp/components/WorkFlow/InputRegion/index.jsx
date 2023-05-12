71f69659f5c8313a7f325814797b8ae942f1722c
chengzihang <czhcheng27@gmail.com>
2023-05-12 21:53:51 +0800

    inputRegion component

--- oa_syetem_client/src/pages/stage/todoList/components/drawerComp/components/WorkFlow/InputRegion/index.jsx
+++ oa_syetem_client/src/pages/stage/todoList/components/drawerComp/components/WorkFlow/InputRegion/index.jsx
@@ -1,16 +1,47 @@
 import React, { useState } from "react";
-import { FullscreenOutlined, PlusSquareFilled } from "@ant-design/icons";
+import { FullscreenOutlined, ExclamationCircleFilled } from "@ant-design/icons";
 import InputOutputWrap from "../InputOutputWrap";
-import noDataIcon from "../assets/pic_nodocument@2x.png";
+import { inputData } from "../mock";
 import SVG2 from "../assets/svg2";
+import ORG from "../assets/org.png";
 import css from "./index.module.less";
 
 const InputRegion = (props) => {
-  const [expand, setExpand] = useState(true);
+  const renderRowTop = (title) => {
+    return (
+      <>
+        <div className={css.oneLineTitleXin}>
+          <img src={ORG} />
+        </div>
+        <div className={css.oneLineTitle}>
+          <span>{title}</span>
+        </div>
+      </>
+    );
+  };
+
+  const renderRowBot = (data) => {
+    return (
+      <>
+        <div
+          title={data.userName + " " + data.orgName}
+          className={css.TwoLineTitle}
+        >
+          <>
+            {data.userName ? "Sender：" + data.userName + " " : null}
+            {data.orgName}
+          </>
+        </div>
+        <div className={css.TwoLineTime}>
+          {data.newTime ? `Due: ${data.newTime}` : ""}
+          {data.newTime ? <ExclamationCircleFilled /> : null}
+        </div>
+      </>
+    );
+  };
   return (
     <InputOutputWrap
       btnPosition="right"
-      callback={setExpand}
       title={
         <div className={css.inputTitle}>
           <div className={css.inputTitleRight}>
@@ -29,9 +60,16 @@ const InputRegion = (props) => {
         </div>
       }
     >
-      <div className={css.no_data}>
-        <img src={noDataIcon} />
-        {expand && <p>No Input</p>}
+      <div className={css.content}>
+        {inputData.map((el, index) => {
+          const { inoutlistDesc, orgName, userName, newTime } = el;
+          return (
+            <div key={index} className={css.input_card}>
+              <div className={css.rowTop}>{renderRowTop(inoutlistDesc)}</div>
+              <div className={css.rowBot}>{renderRowBot(el)}</div>
+            </div>
+          );
+        })}
       </div>
     </InputOutputWrap>
   );
