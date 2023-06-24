f643086fa17954150a4a4debe447133baba8655f
chengzihang <czhcheng27@gmail.com>
2023-06-24 22:56:17 +0800

    change image

--- oa_syetem_client/src/pages/echarts/lineCharts/components/RotateVehicle/index.jsx
+++ oa_syetem_client/src/pages/echarts/lineCharts/components/RotateVehicle/index.jsx
@@ -8,15 +8,54 @@ import React, {
 import css from "./index.module.less";
 
 const RotateVehicle = (props) => {
+  const [initClickX, setInitClickX] = useState(); // 鼠标初次点击页面的 X 位置
+
+  const dragStart = (ev) => {
+    // console.log("dragStart", ev.pageX);
+    setInitClickX(ev.pageX);
+    ev.preventDefault();
+    document.addEventListener("mousemove", movingHandle, { passive: true });
+    document.addEventListener("mouseup", dragStop, {
+      passive: true,
+      once: true,
+    });
+  };
+
+  //  鼠标放开，移除监听
+  const dragStop = () => {
+    document.removeEventListener("mousemove", movingHandle);
+  };
+
+  const movingHandle = (ev) => {
+    // console.log("initClickX", initClickX);
+    const { pageX } = ev;
+    // console.log("pageX", pageX);
+    const img = document
+      .getElementById("imgContentBeatle")
+      .querySelector("img");
+    console.log("img", img);
+    if (pageX > initClickX) {
+      console.log("Right");
+      img.src = require("../../assets/beatle/beatle15.png").default;
+    } else {
+      console.log("Left");
+    }
+  };
+
   return (
     <div className={css.moduleBox}>
-      <div className={css.imgContent}>
+      <div
+        id="imgContentBeatle"
+        className={css.imgContent}
+        // onMouseDown={mouseDown}
+        onMouseDown={dragStart}
+      >
         <img
           src={require("../../assets/beatle/beatle1.png").default}
           alt="beatles"
         />
       </div>
-      <div className={css.rotateDeg}></div>
+      <div className={css.rotateDeg} />
     </div>
   );
 };
