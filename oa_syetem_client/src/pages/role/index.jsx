0a75cb90355107282cac804be349a9ad28333259
chengzihang <czhcheng27@gmail.com>
2023-08-20 15:00:13 +0800

    lottery spin

--- oa_syetem_client/src/pages/role/index.jsx
+++ oa_syetem_client/src/pages/role/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import FlipThreeD from "./FlipThreeD";
 import Carousel3D from "./Carousel3D";
+import LotterySpin from "./LotterySpin";
 import css from "./index.module.less";
 
 const Role = (props) => {
@@ -10,6 +11,9 @@ const Role = (props) => {
       <FlipThreeD />
       <div className={css.carouselBox}>
         <Carousel3D />
+      </div>
+      <div className={css.lotteryBox}>
+        <LotterySpin />
       </div>
     </div>
   );
