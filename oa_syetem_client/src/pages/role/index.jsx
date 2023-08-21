cb802d81257efe603e2104f5e101c755113e0bc5
chengzihang <czhcheng27@gmail.com>
2023-08-21 20:45:29 +0800

    text erase impression

--- oa_syetem_client/src/pages/role/index.jsx
+++ oa_syetem_client/src/pages/role/index.jsx
@@ -4,10 +4,18 @@ import Carousel3D from "./Carousel3D";
 import LotterySpin from "./LotterySpin";
 import css from "./index.module.less";
 
+const text =
+  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor hicarchitecto explicabo animi, optio quaerat, id, fugiat voluptates remodit ipsum ipsam unde incidunt recusandae natus consequunturperferendis repellat totam?";
+
 const Role = (props) => {
   return (
-    <div style={{ height: "100%" }}>
-      Role
+    <div className={css.moduleBox}>
+      <div className={css.container}>
+        <div className={css.textBox}>{text}</div>
+        <div className={css.eraser}>
+          <span className={css.text}>{text}</span>
+        </div>
+      </div>
       <FlipThreeD />
       <div className={css.carouselBox}>
         <Carousel3D />
