165701ddd742155b4f396561878f00644a850683
chengzihang <czhcheng27@gmail.com>
2023-08-22 12:44:43 +0800

    toolBox develop

---
+++ oa_syetem_client/src/pages/role/ToolBox/index.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Icon from "@/components/Icon/index";
+import css from "./index.module.less";
+
+const array = [
+  {
+    iconName: "icon-home",
+    color: "ff2972",
+  },
+  {
+    iconName: "icon-more",
+    color: "fee800",
+  },
+  {
+    iconName: "icon-gift",
+    color: "04fc43",
+  },
+  {
+    iconName: "icon-setting",
+    color: "fe00f1",
+  },
+  {
+    iconName: "icon-message",
+    color: "00b0fe",
+  },
+  {
+    iconName: "icon-cart",
+    color: "fea600",
+  },
+  {
+    iconName: "icon-money-rmb",
+    color: "a529ff",
+  },
+  {
+    iconName: "icon-star",
+    color: "01bdab",
+  },
+];
+
+const ToolBox = (props) => {
+  const renderSpan = () => {
+    return array.map((el, index) => {
+      return (
+        <span style={{ "--i": index, "--clr": `#${el.color}` }}>
+          <Icon iconName={el.iconName} />
+        </span>
+      );
+    });
+  };
+
+  return (
+    <div className={css.menu}>
+      <div className={css.btn}>
+        <Icon iconName="icon-add" />
+      </div>
+      {renderSpan()}
+    </div>
+  );
+};
+
+export default ToolBox;
--- oa_syetem_client/src/pages/role/index.jsx
+++ oa_syetem_client/src/pages/role/index.jsx
@@ -3,9 +3,12 @@ import FlipThreeD from "./FlipThreeD";
 import Carousel3D from "./Carousel3D";
 import LotterySpin from "./LotterySpin";
 import css from "./index.module.less";
+import ToolBox from "./ToolBox";
 
 const text =
   "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor hicarchitecto explicabo animi, optio quaerat, id, fugiat voluptates remodit ipsum ipsam unde incidunt recusandae natus consequunturperferendis repellat totam?";
+// const text =
+//   "我是何姗姗，何姗姗的何，何姗姗的姗，我就是聪明机智善良可爱且自信的何姗姗！哈哈哈哈哈哈哈~";
 
 const Role = (props) => {
   return (
@@ -15,6 +18,9 @@ const Role = (props) => {
         <div className={css.eraser}>
           <span className={css.text}>{text}</span>
         </div>
+      </div>
+      <div className={css.toolBox}>
+        <ToolBox />
       </div>
       <FlipThreeD />
       <div className={css.carouselBox}>
